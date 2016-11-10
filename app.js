6d865be833e83d3a8c1577390f656ccf9ab5fbe4
Spaldin <antonio.stipic2@gmail.com>
2016-11-10 12:07:29 +0100

    Started server

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function (request, response) {
+	response.send("It works!");
+});
+
+app.listen(8888, function () {
+	console.log("Listening on port 8888");
+})
