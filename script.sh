9ec7e7bf1b5e248138a7b89bc93f93e5afab23ce
Spaldin <antonio.stipic2@gmail.com>
2016-11-12 14:30:34 +0100

    Tiny fixes with node_modules

--- script.sh
+++ script.sh
@@ -1,10 +1,11 @@
 npm install express --save
-npm install nodemon --save
-npm install mysql --save
 npm install body-parser --save
+npm install mysql --save
 npm install cookie-parser --save
-npm install connect-redis --save
 npm install crypto --save
 npm install express-session --save
+npm install ejs --save
+
+npm install nodemon --save
 git config user.email "antonio.stipic2@gmail.com"
 git config user.name "Spaldin"
