af822f8b12b25b575028c1ae9769ede083e7eeab
Spaldin <antonio.stipic2@gmail.com>
2016-11-17 10:23:04 +0100

    Edit of db

--- cloude.sql
+++ cloude.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 17, 2016 at 09:50 AM
+-- Generation Time: Nov 17, 2016 at 10:22 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.5.38
 
@@ -47,16 +47,18 @@ INSERT INTO `files` (`id`, `value`, `name`, `extension`, `owner`, `salt`, `uniqu
 (4, 'Hello.txt', 'Hello', 'txt', 'A', 'f27b79390961b69073c83673ef9993c6', '79cd3e176171d8f34119adf5756187d6'),
 (5, 'Small%20hello.txt', 'hello', 'txt', 'A', 'f27b79390961b69073c83673ef9993c6', 'eb6d06b0e88c414488a48d469311ade7'),
 (6, 'test_example.html', 'test_example', 'html', 'A', 'f27b79390961b69073c83673ef9993c6', '08ca446f3f646dd2487c239029f62ef5'),
-(7, 'THIS%20IS%20SECRET', 'secret', 'txt', 'Antonio', '049edf1b8900bf6b7280f71a97700ec7', 'e97b4e4593a3986cdc43b9c7028a7a67'),
+(7, 'THIS%20IS%20SECRET%20FILE', 'secret', 'txt', 'Antonio', '049edf1b8900bf6b7280f71a97700ec7', 'e97b4e4593a3986cdc43b9c7028a7a67'),
 (8, '%7B%0A%20%20%22name%22%3A%20%22cloude%22%2C%0A%20%20%22version%22%3A%20%221.0.0%22%2C%0A%20%20%22description%22%3A%20%22Web%20service%20for%20development%22%2C%0A%20%20%22main%22%3A%20%22app.js%22%2C%0A%20%20%22scripts%22%3A%20%7B%0A%20%20%20%20%22test%22%3A%20%22echo%20%5C%22Error%3A%20no%20test%20specified%5C%22%20%26%26%20exit%201%22%0A%20%20%7D%2C%0A%20%20%22author%22%3A%20%22Antonio%20Stipic%22%2C%0A%20%20%22license%22%3A%20%22ISC%22%2C%0A%20%20%22dependencies%22%3A%20%7B%0A%20%20%20%20%22body-parser%22%3A%20%22%5E1.15.2%22%2C%0A%20%20%20%20%22express%22%3A%20%22%5E4.14.0%22%2C%0A%20%20%20%20%22jsonwebtoken%22%3A%20%22%5E7.1.9%22%2C%0A%20%20%20%20%22mon%22%3A%20%220.0.8%22%2C%0A%20%20%20%20%22mysql%22%3A%20%22%5E2.12.0%22%0A%20%20%7D%0A%7D%0A', 'package', 'json', 'undefined', '11872c7949b4ab1359265ca32f900922', '4718b14eeb2e15cde615343bee878c5b'),
 (9, 'Hello', 'file', 'txt', 'Lara', '82c5d5e22c82265b0504def38c4e87bd', 'f0abe543c194146eeea6669e413ec8ce'),
 (10, 'Facebook%3A%20i''vebeenlookingforawayout', 'lozinke', 'txt', 'Lara', '82c5d5e22c82265b0504def38c4e87bd', '13554aae2a7bbe3e6832fa5f71ff64b5'),
-(13, 'Skillet%20-%20Awake%20and%20Alive%0AEminem%20-%20Mockingbird%0A', 'songs', 'list', 'Antonio', '049edf1b8900bf6b7280f71a97700ec7', 'd9bc93a660836c10dba86031359bf647'),
+(13, 'Skillet%20-%20Awake%20and%20Alive%0AEminem%20-%20Mockingbird%0AAC%2FDC%20-%20Thunderstruck%0A', 'songs', 'list', 'Antonio', '049edf1b8900bf6b7280f71a97700ec7', 'd9bc93a660836c10dba86031359bf647'),
 (14, '12345', 'secret', 'txt', 'A', 'f27b79390961b69073c83673ef9993c6', 'aea94604915b95113ce6509bc8725848'),
 (19, '', '', 'asdf', 'A', 'f27b79390961b69073c83673ef9993c6', '405dd75b0e484b648168369704e7cf0f'),
 (20, 'Hello%20World%20%5C%5C%5C%5C%5C%2F%2F%2F%2F%2F', 'abc', 'txt', 'A', 'f27b79390961b69073c83673ef9993c6', '4f06652f133e15a1c65448ea424cfd40'),
 (21, '12345', 'secret2', 'txt', 'A', 'f27b79390961b69073c83673ef9993c6', '222b06df13d0448c462fd7fb8a90ead0'),
-(23, '%3Chtml%3E%0Ahtmllllllll%0A%0A.....-.%20done', 'vjezba', 'html', 'A', 'f27b79390961b69073c83673ef9993c6', 'a86499ddf83f221b00c4afd27cf7deae');
+(23, '%3Chtml%3E%0Ahtmllllllll%0A%0A.....-.%20done', 'vjezba', 'html', 'A', 'f27b79390961b69073c83673ef9993c6', 'a86499ddf83f221b00c4afd27cf7deae'),
+(24, '-%20Encrypt%20user%20passwords%0A-%20Add%20preferences%20page%0A-%20Edit%20Bootstrap%20CSS', 'cloude-todo', 'list', 'A', 'f27b79390961b69073c83673ef9993c6', 'a63d4836e37a1eee24da122f594b4984'),
+(25, '-%20Encrypt%20user%20passwords%0A-%20Add%20preferences%20page%0A-%20Edit%20Bootstrap%20CSS%0A-%20Add%20folders', 'cloude-todo', 'list', 'Antonio', '049edf1b8900bf6b7280f71a97700ec7', '13d06af9aa2b8bf0e84218e98be7769a');
 
 -- --------------------------------------------------------
 
@@ -108,7 +110,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 --
 -- AUTO_INCREMENT for table `users`
 --
